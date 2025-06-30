084c3bf4cf71c9f64eb2be5e28f06d8daa778bbc
PantACRO4life <6643071+PantACRO4life@users.noreply.github.com>
2025-06-29 23:55:03 -0400

    Update hibeescus.lua

--- OC/Bee/hibeescus.lua
+++ OC/Bee/hibeescus.lua
@@ -3,15 +3,13 @@ local sides = require("sides")
 local os = require("os")
 
 local transposer = component.transposer
--- Use two separate redstone devices
-local redstoneDropper = component.proxy("3df2adb6-4298-4c02-9686-f935cdc48289")
-local redstoneAccelerator = component.proxy("6dc0088e-85e3-4d91-ae67-e62de5549f0e")
+local redstone = component.redstone
 
 -- Configuration
-local chestSide = sides.down        -- Chest is below transposer
-local dropperSide = sides.west      -- Dropper is west of transposer
-local dropperControlSide = sides.down -- Dropper is down from its redstone interface
-local accelSide = sides.west        -- World accelerator is west of its redstone interface
+local chestSide = sides.down           -- Chest is below transposer
+local dropperSide = sides.west         -- Dropper is west of transposer
+local dropperControlSide = sides.down  -- Redstone to dropper (down of computer)
+local accelSide = sides.west           -- Redstone to world accelerator (west of computer)
 
 -- Check if chest has at least one item
 local function chestHasItem()
@@ -35,24 +33,19 @@ while true do
     if moved and moved > 0 then
       print("Moved to dropper.")
 
-      -- Pulse the dropper using its own redstone card
-      redstoneDropper.setOutput(dropperControlSide, 15)
-      os.sleep(0.3)
-      redstoneDropper.setOutput(dropperControlSide, 0)
-      print("Dropper triggered.")
+      redstone.setOutput(dropperControlSide, 15)
+      os.sleep(0.5)
+      redstone.setOutput(dropperControlSide, 0)
+      print("Dropped.")
 
-      -- Turn on accelerator using its own redstone card
-      redstoneAccelerator.setOutput(accelSide, 15)
+      redstone.setOutput(accelSide, 15)
       print("Accelerator ON")
-
-      os.sleep(30) -- run the flower for full conversion time
-
-      redstoneAccelerator.setOutput(accelSide, 0)
+      os.sleep(30)
+      redstone.setOutput(accelSide, 0)
       print("Accelerator OFF")
     end
   else
-    print("Chest is empty. Waiting...")
+    print("Chest is empty, waiting...")
   end
-
-  os.sleep(5)
+  os.sleep(0.5)
 end
